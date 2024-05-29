fa7ab542389f6b6eeccd289d251e30fdb87ab493
Bastien Navarri <navarri.bastien@gmail.com>
2024-05-29 11:10:47 +0200

    recover old simulator

--- src/interfaces/cartesianCoordinate.tsx
+++
@@ -1,5 +0,0 @@
-export interface CartesianCoordinate {
-    x: number;
-    y: number;
-    z: number;
-}
--- src/interfaces/mode.tsx
+++
@@ -1,9 +0,0 @@
-export enum GameMode {
-    SIMULATION = 'SIMULATION',
-    VISUALIZATION = 'VISUALIZATION'
-}
-
-export enum EnvironmentMode {
-    MINECRAFT = "MINECRAFT",
-    COCOBOTS = "COCOBOTS"
-}
